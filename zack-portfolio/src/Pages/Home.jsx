ba4a45e42235b8e06a82298384da7c6448969fe8
Zackery Strickland <Zachary.Strickland@keyin.com>
2024-12-14 22:02:21 -0330

    Commit message

--- zack-portfolio/src/Pages/Home.jsx
+++ zack-portfolio/src/Pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "./style.css";
 
 function Home() {
   return (
     <main>
-      <h2>Welcome to My Website</h2>
-      <p>This is the main content area.</p>
+      <h2>Hello! I'm Zackery Strickland</h2>
+      <p>
+        I'm a passionate <strong>Software Developer</strong> with only about a
+        year of experience and im excited to create visually stunning and
+        user-friendly designs. My journey began with a intrest for art and
+        technology, which led me to pursue a degree in Software Development from
+        Keyin college.
+      </p>
     </main>
   );
 }
